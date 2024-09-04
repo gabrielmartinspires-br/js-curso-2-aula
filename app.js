31ad4677ce057a8ab59af9a3ed0c3d9713579b43
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2024-09-04 04:02:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 let titulo = document.querySelector("h1");
 titulo.innerHTML = "Jogo do número secreto";
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+
+function exibirTextoNaTela() {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
 
 function verificarChute() {
-    console.log('O botão foi clicado!')
+  console.log("O botão foi clicado!");
 }
